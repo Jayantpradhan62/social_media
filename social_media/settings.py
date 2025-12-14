df54a3c4f29b463c1543e5cdb81fd96078d35bd0
Jayant Pradhan <jayantpradhan@J-MacBook-Air.local>
2025-12-15 04:57:59 +0530

    FIxes-9

--- social_media/settings.py
+++ social_media/settings.py
@@ -34,13 +34,14 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "cloudinary",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "cloudinary_storage",
+    "cloudinary",
     "profiles",
     "posts",
     "autho",
